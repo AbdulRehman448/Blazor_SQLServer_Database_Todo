cffc9c8e9fa6a4008dbf8b8a70768462c674f12f
AbdulRehman448 <abdulrehman.tp.786@gmail.com>
2026-06-04 07:25:26 +0500

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Greeting_App.Models;
+
+namespace Greeting_App.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Mapping model to a database table named 'TodoTasks'
+        public DbSet<TodoTask> TodoTasks { get; set; }
+    }
+}
---
+++ Models/TodoTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Greeting_App.Models
+{
+    public class TodoTask
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Task description cannot be empty.")]
+        [StringLength(255)]
+        public string Title { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
